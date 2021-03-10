e2bec9bc6cb8054793f35c9e4c645a1aa5e1e4a8
marcobarilari <marcobarilr@gmail.com>
2021-03-10 14:44:57 +0100

    add code base to play motion sound

--- src/main.m
+++ src/main.m
@@ -2,10 +2,34 @@
 
 % this is the main script to present auditory or visual motion in LePoulpe
 % it calls generate sounds (white, pink, brown noise) in a given length and
-% cuts the sound array into chunks to play each chunk in a speaker. 
+% cuts the sound array into chunks to play each chunk in a speaker.
 
 % for visual motion it activates LEDs in a given speed, plane (horizontal,
 % vertical), and plays them in a given number of repetitions
 
 % if the suer control is needed, please provide that. Otherwise it loops
-% through the repetitions with 5s wait time. 
+% through the repetitions with 5s wait time.
+
+% build the speaker arrays for each direction
+speakerIdxRightward = generateMotionnDirecetionSpeakerArray('rightward')
+
+speakerIdxLeftward = generateMotionnDirecetionSpeakerArray('leftward')
+
+speakerIdxDownward = generateMotionnDirecetionSpeakerArray('downward')
+
+speakerIdxUpward = generateMotionnDirecetionSpeakerArray('upward')
+
+% generateNoise
+
+% cutSound
+
+
+playMotionSound(axis, ...
+                speakerIdx, ...
+                inputSound, ...
+                nbRepetition);
+
+playMotionSound(axis, ...
+                speakerIdx, ...
+                inputSound, ...
+                nbRepetition)
